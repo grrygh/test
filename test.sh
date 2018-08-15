b9f9c0b707cf0d61d520b83de132105ca1ad390b
grrygh <42380969+grrygh@users.noreply.github.com>
2018-08-15 22:08:16 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 readh -p "Input: " varInput
-var1=$(echo 
+var1=$(echo "`curl https://raw.githubusercontent.com/grrygh/test/master/payload`$varInput" | sha256sum)
+
+if [ "$var1" = "$var2" ]; then
+  echo "Hash verified"
+else
+  echo "Hash denied"
+  fi

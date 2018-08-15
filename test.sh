3e28197879b0da5c4760f5084be427c1adaf3b74
grrygh <42380969+grrygh@users.noreply.github.com>
2018-08-15 22:56:39 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-varInput=$1
-var1=$(echo "`curl https://raw.githubusercontent.com/grrygh/test/master/payload`$varInput" | sha256sum)
+#varInput=$1
+var1=$(echo "`curl https://raw.githubusercontent.com/grrygh/test/master/payload`$1" | sha256sum)
 var2=$(curl https://raw.githubusercontent.com/grrygh/test/master/hash)
 
 if [ "$var1" = "$var2" ]; then

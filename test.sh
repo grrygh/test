7ffd4b427908ea1b0642e1f2f4c9f7d773c70f8d
grrygh <42380969+grrygh@users.noreply.github.com>
2018-08-15 22:16:01 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 read -p "Input: " varInput
 var1=$(echo "`curl https://raw.githubusercontent.com/grrygh/test/master/payload`$varInput" | sha256sum)
+var2=$(curl https://raw.githubusercontent.com/grrygh/test/master/hash)
+echo "$var2"
 
 if [ "$var1" = "$var2" ]; then
   echo "Hash verified"
